d138aaface6f4a1ccc661cce097080c9541c7153
Jason <superjar30@gmail.com>
2021-02-21 08:55:57 -0600

    initial commit

---
+++ driver.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+string jsonArrayEditor(string jsonArray)
+{
+    int counter = 1;
+    string newString = "";
+    for(int i = 1; i < jsonArray.length(); i++)
+    {
+        if(jsonArray[i] == '[')
+        {
+            counter++;
+        }
+        else if(jsonArray[i] == ']')
+        {
+            counter--;
+        }
+        if(jsonArray[i] == ']' && counter == 0)
+        {
+             return newString;
+        }
+        else
+        {
+            newString += jsonArray[i];
+        }
+    }
+    return "Please enter a jsonArray represented as a string";
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    string exampleInput = "[{\"fname\":\"val1\", \"lname\": \"val2\", \"sums\":[1,2,3]},{\"fname\":\"val3\", \"lname\":\"val4\"}]";
+    string exampleOutput = jsonArrayEditor(exampleInput);
+    cout << exampleOutput << endl;
+    return 0;
+}
